5cb9b4ff8b786fb8cf5cf206645ffb3842c48c1b
Mohammed Omer Al-Zowa <Alzowa123@gmail.com>
2025-04-29 02:10:12 +0300

    fix:conflict

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 
 import authRouter from "./authRoutes";
+import responseRouter from "./responseRoute";
 
 const router = Router();
 
 // Register all routes
 router.use("/auth", authRouter);
+router.use("/responses", responseRouter);
 
 export default router;
