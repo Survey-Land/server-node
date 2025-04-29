7b809be4569ada4d883e72b4044f944f6bfd15e6
lakhdar <lkboudjahfa@gmail.com>
2025-04-29 19:06:18 +0100

    feat:add refresh token

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,160 +1,151 @@
 import { Request, Response, NextFunction } from "express";
 import { AuthService } from "../services/authService";
-import { generateToken } from "../utils/jwt.util";
+import {
+  generateRefetchToken,
+  generateToken,
+  verifyRefreshToken,
+} from "../utils/jwt.util";
 import i18n from "../config/i18n";
-import passport from "passport";
-
+import { JwtPayload } from "../types/global";
 export class AuthController {
-    private authService = new AuthService();
+  private authService = new AuthService();
 
-    private setLocale(req: Request) {
-        const lang = req.headers["accept-language"] || "ar";
-        i18n.setLocale(lang as string);
-        return lang;
+  private setLocale(req: Request) {
+    const lang = req.headers["accept-language"] || "ar";
+    i18n.setLocale(lang as string);
+    return lang;
+  }
+
+  findAll = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const lang = this.setLocale(req);
+      const users = await this.authService.findAll(req.query, lang);
+      res.json(users);
+    } catch (e) {
+      next(e);
     }
+  };
 
-    findAll = async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const lang = this.setLocale(req);
-            const users = await this.authService.findAll(req.query, lang);
-            res.json(users);
-        } catch (e) {
-            next(e);
-        }
-    };
+  register = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const lang = this.setLocale(req);
+      const { email, password, name } = req.body;
 
-    register = async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const lang = this.setLocale(req);
-            const { email, password, name } = req.body;
-            const user = await this.authService.register(
-                { email, password, name },
-                lang
-            );
-            const token = generateToken({
-                id: user.id,
-                email: user.email,
-                role: user.role,
-            });
-            res.status(201).json({
-                message: i18n.__("User registered successfully"),
-                token,
-                user: {
-                    id: user.id,
-                    email: user.email,
-                    name: user.name,
-                    role: user.role,
-                },
-            });
-        } catch (e) {
-            next(e);
-        }
-    };
+      const user = await this.authService.register(
+        { email, password, name },
+        lang
+      );
+      const token = generateToken({
+        id: user.id,
+        email: user.email,
+        role: user.role,
+      });
 
-    login = async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const lang = this.setLocale(req);
-            const { email, password } = req.body;
-            const user = await this.authService.login(email, password, lang);
-            const token = generateToken({
-                id: user.id,
-                email: user.email,
-                role: user.role,
-            });
-            res.json({
-                message: i18n.__("Logged in successfully"),
-                token,
-                user: {
-                    id: user.id,
-                    email: user.email,
-                    name: user.name,
-                    role: user.role,
-                },
-            });
-        } catch (e) {
-            next(e);
-        }
-    };
-
-    profile = async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            this.setLocale(req);
-            const { id } = req.user as { id: string };
-            const profile = await this.authService.getProfile(id);
-            res.json(profile);
-        } catch (e) {
-            next(e);
-        }
-    };
-
-    logout = (_req: Request, res: Response) => {
-        this.setLocale(_req);
-        res.json({ message: i18n.__("Logged out successfully") });
-    };
-
-    googleLogin = (req: Request, res: Response, next: NextFunction) => {
-        passport.authenticate("google", { scope: ["profile", "email"] })(
-            req,
-            res,
-            next
-        );
-    };
-
-    googleCallback = (req: Request, res: Response, next: NextFunction) => {
-        passport.authenticate("google", { session: false }, (err, user) => {
-            if (err || !user)
-                return res
-                    .status(400)
-                    .json({ message: i18n.__("Authentication failed") });
-            const token = generateToken({
-                id: user.id,
-                email: user.email,
-                role: user.role,
-            });
-            res.json({
-                message: i18n.__("Logged in successfully with Google"),
-                token,
-                user: {
-                    id: user.id,
-                    email: user.email,
-                    name: user.name,
-                    role: user.role,
-                },
-            });
-        })(req, res, next);
-    };
-
-    githubLogin(req: Request, res: Response, next: NextFunction) {
-        passport.authenticate("github", { scope: ["user:email"] });
+      res.status(201).json({
+        message: i18n.__("User registered successfully"),
+        token,
+        user: {
+          id: user.id,
+          email: user.email,
+          name: user.name,
+          role: user.role,
+        },
+      });
+    } catch (e) {
+      next(e);
     }
-    githubCallback = (req: Request, res: Response, next: Function) => {
-        passport.authenticate(
-            "github",
-            { session: false },
-            (err: any, user: any, _info: any) => {
-                if (err || !user) {
-                    return res
-                        .status(400)
-                        .json({ message: i18n.__("Authentication failed") });
-                }
+  };
 
-                const token = generateToken({
-                    id: user.id,
-                    email: user.email,
-                    role: user.role,
-                });
+  login = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const lang = this.setLocale(req);
+      const { email, password } = req.body;
 
-                res.json({
-                    message: i18n.__("Logged in successfully with GitHub"),
-                    token,
-                    user: {
-                        id: user.id,
-                        email: user.email,
-                        name: user.name,
-                        role: user.role,
-                    },
-                });
-            }
-        )(req, res, next);
-    };
+      const user = await this.authService.login(email, password, lang);
+
+      const accessToken = generateToken({
+        id: user.id,
+        email: user.email,
+        role: user.role,
+      });
+
+      const refreshToken = generateRefetchToken({
+        id: user.id,
+        email: user.email,
+        role: user.role,
+      });
+
+      const expiry = new Date();
+
+      expiry.setDate(expiry.getDate() + 7);
+
+      res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secure: false,
+        path: "/api/auth/refresh",
+        sameSite: "strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+      });
+
+      res.json({
+        message: i18n.__("Logged in successfully"),
+        accessToken,
+        user: {
+          id: user.id,
+          email: user.email,
+          name: user.name,
+          role: user.role,
+        },
+      });
+    } catch (e) {
+      next(e);
+    }
+  };
+
+  refreshToken = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const refreshToken = req.cookies.refreshToken;
+
+      if (!refreshToken) {
+        throw new Error("Refresh token is missing.");
+      }
+
+      const user = verifyRefreshToken(refreshToken) as JwtPayload;
+
+      if (!user) {
+        throw new Error("Invalid refresh token payload.");
+      }
+
+      const accessToken = generateToken({
+        id: user.id,
+        email: user.email,
+        role: user.role,
+      });
+
+      res.status(200).json({
+        success: true,
+        message: "Access token refreshed successfully.",
+        accessToken,
+      });
+    } catch (e) {
+      next(e);
+    }
+  };
+
+  profile = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      this.setLocale(req);
+      const { id } = req.user as { id: string };
+      const profile = await this.authService.getProfile(id);
+      res.json(profile);
+    } catch (e) {
+      next(e);
+    }
+  };
+
+  logout = (req: Request, res: Response): void => {
+    this.setLocale(req);
+
+    res.json({ message: i18n.__("Logged out successfully") });
+  };
 }
--- src/utils/jwt.util.ts
+++ src/utils/jwt.util.ts
@@ -1,14 +1,23 @@
 // src/utils/jwt.util.ts
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 export function generateToken(payload: object): string {
   if (!process.env.JWT_SECRET) {
-    throw new Error('JWT_SECRET is not defined in environment variables');
+    throw new Error("JWT_SECRET is not defined in environment variables");
   }
 
-  return jwt.sign(
-    payload,
-    process.env.JWT_SECRET,
-    { expiresIn: '30d' } 
-  );
+  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "15M" });
+}
+
+export function generateRefetchToken(payload: object): string {
+  if (!process.env.REFRESH_TOKEN) {
+    throw new Error("REFRESH_TOKEN is not defined in environment variables");
+  }
+
+  return jwt.sign(payload, process.env.REFRESH_TOKEN, { expiresIn: "30d" });
+}
+
+export function verifyRefreshToken(refreshToken: string) {
+  const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN as string);
+  return decoded;
 }
