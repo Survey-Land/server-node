97bbbdd223ad823d462a9dcd178f3621bac09ba8
Mohammed Omer Al-Zowa <Alzowa123@gmail.com>
2025-05-02 23:10:33 +0300

    fix:build

--- src/middleware/not-foundMiddleware.ts
+++ src/middleware/not-foundMiddleware.ts
@@ -1,6 +1,17 @@
 import { Request, Response, NextFunction } from 'express';
 import createError from 'http-errors';
 
-export const notFoundHandler = (req: Request, res: Response, next: NextFunction) => {
+export const notFoundHandler = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (req.path === '/' || req.originalUrl === '/') {
+    return res.status(200).json({
+      status: 200,
+      message: 'Backend Server is Running 🚀'
+    });
+  }
+
   next(createError(404, 'الصفحة غير موجودة'));
 };
