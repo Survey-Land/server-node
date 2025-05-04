25611a193ced6537d0d5a6c7c8ff1ff2216fbf1f
Youssef Tarek <yossef.tarekelsheikh@gmail.com>
2025-05-05 02:38:46 +0300

    final changes in question routes

--- src/routes/questionRoute.ts
+++ src/routes/questionRoute.ts
@@ -9,7 +9,7 @@ const controller = new QuestionObjectController();
 
 router.post("/:surveyId", authenticateJWT, validate(createQuestionSchema), controller.addQuestion);
 router.get("/:surveyId", authenticateJWT, controller.getQuestions);
-router.put("/:surveyId/questions/:qid", authenticateJWT, validate(updateQuestionSchema), controller.updateQuestion);
-router.delete("/:surveyId/questions/:qid", authenticateJWT, controller.deleteQuestion);
+router.put("/:surveyId/qid/:qid", authenticateJWT, validate(updateQuestionSchema), controller.updateQuestion);
+router.delete("/:surveyId/qid/:qid", authenticateJWT, controller.deleteQuestion);
 
 export default router;
