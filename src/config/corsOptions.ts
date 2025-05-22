684678306b1de29416cd838178c767a081b32ee8
Mohammed Omer Al-Zowa <Alzowa123@gmail.com>
2025-05-22 08:27:21 +0300

    fix:cors

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -2,7 +2,7 @@ import cors from "cors";
 const corsOptions: cors.CorsOptions = {
   origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], // Specify which methods are allowed
-  allowedHeaders: ["Content-Type", "Authorization", "Accept-Language"], // Specify which headers are allowed
+  allowedHeaders: ["Content-Type", "Authorization", "accept-language"], // Specify which headers are allowed
   credentials: true, // Allow credentials (cookies, authorization headers, etc.)
   exposedHeaders: ["accept-language"],
 };
